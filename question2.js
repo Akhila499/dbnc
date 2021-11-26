cba60f3d70dea133388be754444fc7b5f901c91d
Akhila499 <akhilathota499@gmail.com>
2021-11-26 13:49:09 -0500

    complete question 2

--- question2.js
+++ question2.js
@@ -4,3 +4,23 @@
 // c) it prints "foobar" if NUMBER is divisible by 14;
 // d) it prints NUMBER value for other cases;
 // note: NUMBER is a positive integer number;
+
+function printText(arg) {
+  let output = "";
+  if (arg % 14 === 0) {
+    output = "foobar";
+    return output;
+  }
+  if (arg % 2 === 0) {
+    output = "foo";
+    return output;
+  }
+  if (arg % 7 === 0) {
+    output = "bar";
+    return output;
+  }
+  output = arg;
+  return output;
+}
+
+console.log(printText(140));
